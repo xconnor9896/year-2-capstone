94e8f5a1400d72666458e4e5456fcf9b770deb0b
Connor Kobriger <cjkobriger@gmail.com>
2022-03-24 14:08:13 -0700

    made some very small negligible changes

--- pages/reports.jsx
+++ pages/reports.jsx
@@ -2,10 +2,10 @@ import Navbar from "../components/Navbar";
 import styles from "../styles/pages/Reports.module.scss";
 
 export default function Reports() {
-	return (
-		<main className={styles.container}>
-            <Navbar/>
-			<article className={styles.main}>VIEW ALL REPORTS</article>
-		</main>
-	);
+  return (
+    <main className={styles.container}>
+      <Navbar />
+      <article className={styles.main}>VIEW ALL REPORTS</article>
+    </main>
+  );
 }
