0df3dd86004fe886eda6b8d8d363d57bb149056d
Nick Sachau <nicksachau156@gmail.com>
2022-03-25 14:23:05 -0700

    test

---
+++ pages/Dashboardashdafkajdfsdf.jsx
@@ -0,0 +1,31 @@
+import Navbar from '../components/Navbar'
+import { FaRegUser, FaUpload, FaRegIdBadge } from 'react-icons/fa'
+
+const Dashboard = () => {
+  const name = 'John'
+  const rank = 'Lt.'
+
+  return (
+    <>
+      <Navbar />
+      <div className="dashboard">
+        <div className="profile">
+          <h1 className="title">Welcome Back!</h1>
+          <h2 className="name">{rank} {name}!</h2>
+          <div className="pp">
+            <FaRegUser />
+          </div>
+          <div className="name2">{rank} {name}</div>
+          <button id="newReport"><FaUpload /> Make a new report</button>
+          <button id="profile"><FaRegIdBadge /> My Profile</button>
+        </div>
+        <div className="divider"></div>
+        <div className="cases">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Dashboard
