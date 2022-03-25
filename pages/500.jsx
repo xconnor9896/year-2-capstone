d96a18ce9898449f47c24107a333d7fc6bf93a11
NotTimTam <timmyiscool8@gmail.com>
2022-03-25 16:09:08 -0700

    Finished error pages.

---
+++ pages/500.jsx
@@ -0,0 +1,25 @@
+import styles from "../styles/pages/Error.module.scss";
+import { Button } from "../proton";
+import { useRouter } from "next/router";
+
+export default function Custom500() {
+	const router = useRouter();
+
+	const navTo = (url) => {
+		router.push(url);
+	};
+
+	return (
+		<main className={`${styles.container} ${styles.warning}`}>
+			<article className={styles.main}>
+				<h1>500</h1>
+				<h2>Something went wrong on our end</h2>
+				<p>
+					Something broke server-side and it isn't your fault. Please
+					try again later.
+				</p>
+				<Button onClick={() => navTo("/dashboard")}>Dashboard</Button>
+			</article>
+		</main>
+	);
+}
