37b35f25b367515baeca150e878dc74c31b54cc1
Sean Zimbelman <Szimbe295@west-mec.local>
2022-03-25 17:52:01 -0700

    delete and change username

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,8 +4,15 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const isEmail = require("validator/lib/isEmail");
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+CREATE USER
+.post('/signup') 
+req.body {user} //? The new user in a user object
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 const createUser = async (req, res) => {
-  const { name, email, badgeNumber, squadNumber, password } = req.body.user;
+  const { name, email, badgeNumber, squadNumber, password, rank, username } =
+    req.body.user;
 
   if (!isEmail(email)) return res.status(401).send("Invalid");
   if (password.length < 8) {
@@ -29,6 +36,8 @@ const createUser = async (req, res) => {
       profilePic: rec.body.profilePic || defaultProfilePic,
       badgeNumber,
       squadNumber: squadNumber || 0,
+      rank,
+      username,
     });
 
     user.password = bcrypt.hash(password, 10);
@@ -50,8 +59,14 @@ const createUser = async (req, res) => {
   }
 };
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+LOGIN USER
+.post('/login') 
+req.body { email, password } //? Your email and password
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 const loginUser = async (req, res) => {
-  const { email, password } = req.body.user;
+  const { email, password } = req.body;
 
   if (!isEmail(email)) return res.status(401).send("Invalid Email");
   if (password.length < 8)
@@ -84,4 +99,59 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser, loginUser };
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+DELETE USER
+.delete('/:userId') 
+req.params { userId } //? Your Id
+req.body { _id } //? Targets Id
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+const deleteUser = async (req, res) => {
+  const { _id } = req.body;
+  const { userId } = req.params;
+
+  try {
+    const user = UserModel.findById(userId);
+
+    if (user.rank !== "captain") {
+      return res
+        .status(401)
+        .send("Please contact your captain about deleting your account");
+    }
+
+    const deleted = UserModel.deleteOne({ _id });
+
+    if (deleted) {
+      return res.status(200).send("User Deleted");
+    } else {
+      return res.status(404).send("User Not Found");
+    }
+  } catch (error) {
+    console.log("error at deleteUser controller");
+    console.log(error);
+  }
+};
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+CHANGE USERNAME
+.delete('/:userId') 
+req.params {userId} //? Targets Id
+req.body {username} //? New Username
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+const changeUsername = async (req, res) => {
+  const { username } = req.body;
+  const { userId } = req.params;
+
+  try {
+    let user = UserModel.findById(userId);
+    user.username = username;
+    user = await user.save();
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log("error at changeUsername controller");
+    console.log(error);
+  }
+};
+module.exports = { createUser, loginUser, deleteUser, changeUsername };
