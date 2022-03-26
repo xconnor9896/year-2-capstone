e31a3240f1bf24a17f7c10483124ab478f48bed1
NotTimTam <timmyiscool8@gmail.com>
2022-03-25 18:02:34 -0700

    Heavy work on login/signup page.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,18 @@
 import styles from "../styles/pages/Home.module.scss";
 import { Button } from "../proton";
 import { useState } from "react";
-import { FaSignInAlt, FaUserPlus, FaUser, FaLock } from "react-icons/fa";
+import {
+	FaSignInAlt,
+	FaUserPlus,
+	FaEnvelope,
+	FaLock,
+	FaCheckDouble,
+	FaArrowLeft,
+	FaArrowRight,
+} from "react-icons/fa";
 import Input from "../components/Input";
 
-const LoginPage = ({ setLogin }) => {
+const LoginPage = ({ setState }) => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
@@ -14,15 +22,18 @@ const LoginPage = ({ setLogin }) => {
 	return (
 		<>
 			<form onSubmit={handleSubmit}>
+				<h1>LOGIN</h1>
 				<Input
-					icon={<FaUser />}
-					type="text"
-					name="username"
-					id="username"
-					placeholder="Username"
+					required
+					icon={<FaEnvelope />}
+					type="email"
+					name="email"
+					id="email"
+					placeholder="Email"
 				/>
 
 				<Input
+					required
 					icon={<FaLock />}
 					type="password"
 					name="password"
@@ -31,33 +42,179 @@ const LoginPage = ({ setLogin }) => {
 				/>
 				<Button type="submit" emphasis="primary">
 					<FaSignInAlt />
-					Log In
+					Login
 				</Button>
 			</form>
-			<div className={styles.reRoute}>
-				<p>New around here?</p>
+
+			<div className={styles.section}>
+				<div className={styles.reRoute}>
+					<p>New around here?</p>
+					<Button
+						compact
+						emphasis="secondary"
+						onClick={() => setState("sign_up")}
+					>
+						<FaUserPlus />
+						Sign Up
+					</Button>
+				</div>
 				<Button
 					compact
 					emphasis="secondary"
-					onClick={() => setLogin(false)}
+					onClick={() => setState("recovery")}
 				>
-					<FaUserPlus />
-					Sign Up
+					Forgot Password?
 				</Button>
 			</div>
 		</>
 	);
 };
-const SignUpPage = ({ setLogin }) => {
+
+const RecoveryPage = ({ setState }) => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log("implement handleSubmit for recovery page...");
+	};
+
 	return (
 		<>
-			<p>SIGNUP PAGE</p>
+			<form onSubmit={handleSubmit}>
+				<h1>RECOVER PASSWORD</h1>
+				<Input
+					required
+					icon={<FaEnvelope />}
+					type="email"
+					name="email"
+					id="email"
+					placeholder="Email"
+				/>
+
+				<Button type="submit" emphasis="primary">
+					<FaLock />
+					Submit
+				</Button>
+			</form>
+
+			<div className={styles.section}>
+				<Button
+					compact
+					emphasis="secondary"
+					onClick={() => setState("login")}
+				>
+					<FaArrowLeft />
+					Back to Login
+				</Button>
+			</div>
+		</>
+	);
+};
+
+const SignUpPage = ({ setState }) => {
+	const [step, setStep] = useState(1);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log("implement handleSubmit for signup page...");
+	};
+
+	return (
+		<>
+			<form onSubmit={handleSubmit} step={step}>
+				<h1>SIGN UP</h1>
+				<div className={`${styles.step} ${styles.step1}`}>
+					<Input
+						icon={<FaEnvelope />}
+						type="email"
+						name="email"
+						id="email"
+						placeholder="Email"
+					/>
+
+					<Input
+						icon={<FaLock />}
+						type="text"
+						name="password"
+						id="password"
+						placeholder="Password"
+					/>
+					<Input
+						icon={<FaCheckDouble />}
+						type="text"
+						name="password-confirm"
+						id="password-confirm"
+						placeholder="Confirm Password"
+					/>
+
+					<Button.Group>
+						<Button
+							type="submit"
+							emphasis="secondary"
+							compact
+							onClick={() => setStep(2)}
+						>
+							<FaArrowRight />
+							Next
+						</Button>
+					</Button.Group>
+				</div>
+
+				<div className={`${styles.step} ${styles.step2}`}>
+					<p>PROFILE PICTURE</p>
+					<p>PREFIX DROPDOWN</p>
+					<p>STUDENT ID (BADGE NUMBER)</p>
+
+					<Button.Group>
+						<Button
+							type="submit"
+							emphasis="primary"
+							compact
+							onClick={() => setStep(1)}
+						>
+							<FaArrowLeft />
+							Back
+						</Button>
+						<Button
+							type="submit"
+							emphasis="secondary"
+							compact
+							onClick={() => setStep(3)}
+						>
+							<FaArrowRight />
+							Next
+						</Button>
+					</Button.Group>
+				</div>
+
+				<div className={`${styles.step} ${styles.step3}`}>
+					<p>SQUAD</p>
+					<p>RANK</p>
+
+					<Button.Group>
+						<Button
+							type="submit"
+							emphasis="secondary"
+							compact
+							onClick={() => setStep(2)}
+						>
+							<FaArrowLeft />
+							Back
+						</Button>
+					</Button.Group>
+
+					<Button type="submit" emphasis="primary">
+						<FaUserPlus />
+						Sign Up
+					</Button>
+				</div>
+			</form>
 			<div className={styles.reRoute}>
 				<p>Already a user?</p>
 				<Button
 					compact
 					emphasis="secondary"
-					onClick={() => setLogin(true)}
+					onClick={() => setState("login")}
 				>
 					<FaSignInAlt />
 					Login
@@ -68,42 +225,59 @@ const SignUpPage = ({ setLogin }) => {
 };
 
 export default function Home() {
-	const [login, setLogin] = useState(true);
+	const [state, setState] = useState("login");
 
 	return (
 		<main className={styles.container}>
 			<article className={styles.main}>
 				<aside>
 					<h1>
-						<span>Welcome{login && " Back"}!</span>
+						{(state === "login" || state === "sign_up") && (
+							<span>
+								Welcome
+								{state === "login" && " Back"}!
+							</span>
+						)}
 
-						{login && (
+						{state === "login" && (
 							<span>
 								Ready to make a new <b>report</b>?
 							</span>
 						)}
 
-						<span>
-							{login ? (
+						{state === "recovery" && (
+							<span>
+								Password <b>Recovery</b>
+							</span>
+						)}
+
+						{state === "login" && (
+							<span>
 								<p>
 									please <b>login</b>
 								</p>
-							) : (
+							</span>
+						)}
+
+						{state === "sign_up" && (
+							<span>
 								<p>
 									<b>sign up</b> to get started.
 								</p>
-							)}
-						</span>
+							</span>
+						)}
 					</h1>
 				</aside>
 				<section>
 					<div className={styles.lpsLogo}>
 						<img alt="LPS Logo" src="./LPS Logo.svg" />
 					</div>
-					{login ? (
-						<LoginPage {...{ setLogin }} />
+					{state === "login" ? (
+						<LoginPage {...{ setState }} />
+					) : state === "sign_up" ? (
+						<SignUpPage {...{ setState }} />
 					) : (
-						<SignUpPage {...{ setLogin }} />
+						<RecoveryPage {...{ setState }} />
 					)}
 				</section>
 			</article>
