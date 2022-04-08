36faf754110c353e334e764e1374a6788f4b3592
Ethaniel Botello <Ebotel200@west-mec.local>
2022-04-08 13:32:45 -0700

    profile

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -2,17 +2,25 @@ import { FaRegUser, FaUpload, FaRegIdBadge, FaEye, FaPrint } from 'react-icons/f
 import styles from "../../styles/Pages/Profile.module.scss";
 
 export default function Profile() {
-	const name = 'John'
+	const name = 'John Doe'
+	const badgeNum = '297502'
 	const rank = 'Lt.'
 	const caseId = '1992197'
-	
+	const squad = 'K-9Unit'
+	const squadNum = '593'
 
 	return (
 		<main className={styles.container}>
-			<article className={styles.main}>USER PROFILE</article>
-			
-			
+			<div className="user">
+				<div className="pfp"><img src="" alt="" /></div>
 
+				<ul>
+					<li className={name}></li>
+					<li className={badgeNum}></li>
+					<li className={rank}></li>
+					<li className={squadNum}></li>
+				</ul>
+			</div>
 		</main>
 	);
 }
