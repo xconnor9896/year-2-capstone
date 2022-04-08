bd3d993642208fb680f8d97d18cc99fa6fdf5e3d
Ethaniel Botello <Ebotel200@west-mec.local>
2022-04-08 15:25:17 -0700

    profile

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,26 +1,39 @@
-import { FaRegUser, FaUpload, FaRegIdBadge, FaEye, FaPrint } from 'react-icons/fa'
+import {
+  FaRegUser,
+  FaUpload,
+  FaRegIdBadge,
+  FaEye,
+  FaPrint,
+} from "react-icons/fa";
+import { MdOutlineLocalPolice, MdOutlineGroups } from "react-icons/Md";
+import { AiOutlineStar } from "react-icons/Ai";
+
 import styles from "../../styles/Pages/Profile.module.scss";
 
 export default function Profile() {
-	const name = 'John Doe'
-	const badgeNum = '297502'
-	const rank = 'Lt.'
-	const caseId = '1992197'
-	const squad = 'K-9Unit'
-	const squadNum = '593'
+  const name = "John Doe";
+  const badgeNum = "297502";
+  const rank = "Lt.";
+  const caseId = "1992197";
+  const squad = "K-9Unit";
+  const squadNum = "593";
 
-	return (
-		<main className={styles.container}>
-			<div className="user">
-				<div className="pfp"><img src="" alt="" /></div>
-
-				<ul>
-					<li className={name}></li>
-					<li className={badgeNum}></li>
-					<li className={rank}></li>
-					<li className={squadNum}></li>
-				</ul>
-			</div>
-		</main>
-	);
+  return (
+    <main className={styles.container}>
+      <div className={styles.user}>
+        <div className={styles.pfp}>
+          <FaRegUser />
+        </div>
+        <ul>
+          <li className={styles.name}>{name}</li>
+          <li className={styles.badgeNum}>
+            <MdOutlineLocalPolice />
+            {badgeNum}
+          </li>
+          <li className={styles.rank}><AiOutlineStar/>{rank}</li>
+          <li className={styles.squadNum}><MdOutlineGroups/>{squadNum}</li>
+        </ul>
+      </div>
+    </main>
+  );
 }
