6bef57d8d48e51559ba5a7d77dd68cc7346c78bd
Connor Kobriger <cjkobriger@gmail.com>
2022-04-13 13:31:33 -0700

    mostly finished UserModel.js

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -3,7 +3,46 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  
-});
+  name: {
+    type: String,
+    required: true,
+  },
+  badge: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  squad: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    // verification from either west-mec.org or west-mec.edu goes here
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  profilePicURL: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ["officer", "captain", "teacher", "admin"],
+    default: 'officer'
+  }
+},
+  // {timestamps: true} |||| COMMENTING THIS OUT, NOT SURE IF WE INCLUDE OR NOT
+);
 
 export default mongoose.model('User', UserSchema);
