eb620743c2a04fc549a6ee586219b5d5d907700c
David Nevarez <david.nevarez03@hotmail.com>
2022-04-14 16:30:59 -0700

    updated routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,19 +1,12 @@
 const router = require(`express`).Router();
 
 const { createUser,
-  loginUser,
-  deleteUser,
-  updateUser,
-  changePassword } = require("../controllers/user");
+  loginUser } = require("../controllers/user");
 
 router.post('/signup', createUser);
 router.post('/login', loginUser);
 
-router.delete('/:userId', deleteUser);
 
-router.post('/:userId', updateUser);
-
-router.post('/', changePassword);
 
 
 
