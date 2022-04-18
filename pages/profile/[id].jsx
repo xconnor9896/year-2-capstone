cf80473dde71084eaeba7ed209991eec667c0bcb
Ethaniel Botello <Ebotel200@west-mec.local>
2022-04-18 15:28:31 -0700

    more styles

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -5,8 +5,9 @@ import {
   FaEye,
   FaPrint,
 } from "react-icons/fa";
-import { MdOutlineLocalPolice, MdOutlineGroups } from "react-icons/Md";
-import { AiOutlineStar } from "react-icons/Ai";
+import { FaStar } from "react-icons/Fa";
+
+//! This is just a placeholder for the icons there will be more soon
 
 import styles from "../../styles/Pages/Profile.module.scss";
 
@@ -18,21 +19,84 @@ export default function Profile() {
   const squad = "K-9Unit";
   const squadNum = "593";
 
+  const view = true;
+
   return (
     <main className={styles.container}>
       <div className={styles.user}>
         <div className={styles.pfp}>
           <FaRegUser />
         </div>
-        <ul>
-          <li className={styles.name}>{name}</li>
-          <li className={styles.badgeNum}>
-            <MdOutlineLocalPolice />
-            {badgeNum}
-          </li>
-          <li className={styles.rank}><AiOutlineStar/>{rank}</li>
-          <li className={styles.squadNum}><MdOutlineGroups/>{squadNum}</li>
-        </ul>
+        {/* ======================================================= */}
+        <div className={styles.miniNav}>
+          <ul>
+            <li><div></div><h2>Info</h2></li>
+            <li><div></div><h2>Contact</h2></li>
+            <li><div></div><h2>Cases</h2></li>
+          </ul>
+        </div>
+        {/* ======================================================= */}
+        <div className={styles.studentInfo}>
+          <ul>
+            <li>
+              <h3>
+                <FaStar /> {name}
+              </h3>
+            </li>
+            <li>
+              <h3>
+                <FaStar /> {badgeNum}
+              </h3>
+            </li>
+            <li>
+              <h3>
+                <FaStar /> {rank}
+              </h3>
+            </li>
+            <li>
+              <h3>
+                <FaStar /> {squad}
+              </h3>
+            </li>
+            <li>
+              <h3>
+                <FaStar /> {squadNum}
+              </h3>
+            </li>
+          </ul>
+        </div>
+        {/* ======================================================= */}
+        <div className={styles.contactInfo}>
+          <ul>
+            <li>
+              <h3>
+                <FaStar /> {name}
+              </h3>
+            </li>
+            <li>
+              <h3>
+                <FaStar /> {badgeNum}
+              </h3>
+            </li>
+            <li>
+              <h3>
+                <FaStar /> {rank}
+              </h3>
+            </li>
+            <li>
+              <h3>
+                <FaStar /> {squad}
+              </h3>
+            </li>
+            <li>
+              <h3>
+                <FaStar /> {squadNum}
+              </h3>
+            </li>
+          </ul>
+        </div>
+        {/* ======================================================= */}
+        <div className={styles.display}></div>
       </div>
     </main>
   );
