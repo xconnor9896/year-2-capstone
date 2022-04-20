41ee8b562bd0a7f592db1d1f33dc9498c57d67c5
David Nevarez <david.nevarez03@hotmail.com>
2022-04-19 18:21:44 -0700

    David's commit for the day

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ app.use(express.json());
 
 //routerss
 
+const userRoutes = require("./server/routes/userRoutes");
+const reportRoutes = require("./server/routes/reportRoutes");
+const settingsRoutes = require("./server/routes/settingsRoutes");
+
+// routes
+
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/report", reportRoutes);
+app.use("/api/v1/settings", settingsRoutes);
+
 //conect to database
 connectDB();
 
