6de7014896bb65ae83d8fa8d4d2def66b7e27329
Zach <ztaylo273@west-mec.org>
2022-04-28 15:23:16 -0700

    Fished the bulk of the emailCon

--- server/routes/emailRoutes.js
+++ server/routes/emailRoutes.js
@@ -2,6 +2,6 @@ const router = require(`express`).Router();
 
 const { sendVerfEmail, sendPassResetEmail, getUserEmail} = require("../controllers/emailCon")
 
-router.route("/").get(getUserEmail)
+router.route("/").get(sendVerfEmail)
 
 module.exports =  router 
