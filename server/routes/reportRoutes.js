1c42a6bb79afb9855046e81473d15c110595fef4
A <zimbiekitten@gmail.com>
2022-04-29 18:27:29 -0700

    getReport fix

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -15,13 +15,9 @@ const {authMiddleware} = require('../middleware/authMidware')
 
 
 router.post('/', authMiddleware, createReport);
-router.get('/', authMiddleware, getAllReports);
-
-
+router.post('/all', authMiddleware, getAllReports);
 router.delete('/:reportId', authMiddleware, deleteReport);
 router.get('/:reportId/:userId', authMiddleware, getReport);
-
-
 router.post('/:reportId', authMiddleware, updateReport);
 router.post('/verify/:reportId', authMiddleware, verifyReport);
 router.post('/importance/:reportId', authMiddleware, importanceReport);
